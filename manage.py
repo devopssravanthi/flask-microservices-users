76f76b66cb852bca7c91bc2850afae8687de457c
devopssravanthi <42515872+devopssravanthi@users.noreply.github.com>
2019-08-22 18:55:43 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@
 import unittest
 import coverage
 
-from flask.ext.script import Manager
+from flask_script import Manager
 from flask_migrate import MigrateCommand
 
 from project import create_app, db
