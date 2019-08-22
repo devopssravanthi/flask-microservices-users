a20b93427b3a4da610aad5b4f60b73012d677b7a
devopssravanthi <42515872+devopssravanthi@users.noreply.github.com>
2019-08-22 18:59:13 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,8 +4,11 @@
 import unittest
 import coverage
 
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from flask_migrate import MigrateCommand
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from project import create_app, db
 from project.api.models import User
